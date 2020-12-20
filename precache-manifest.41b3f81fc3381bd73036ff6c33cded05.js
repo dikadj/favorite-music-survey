9040a290fe1f4a37bbcbe61dbc22f343d86162e6
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-20 22:37:51 +0700

    Updates

---
+++ precache-manifest.41b3f81fc3381bd73036ff6c33cded05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb8fa76729f94048221b",
+    "url": "/favorite-music-survey/static/css/main.5afc792f.chunk.css"
+  },
+  {
+    "revision": "eb8fa76729f94048221b",
+    "url": "/favorite-music-survey/static/js/main.28f35278.chunk.js"
+  },
+  {
+    "revision": "4dec0fdb1b52a96c4191",
+    "url": "/favorite-music-survey/static/js/runtime~main.4d338cc9.js"
+  },
+  {
+    "revision": "918994b293c26999f84c",
+    "url": "/favorite-music-survey/static/js/2.509fda2e.chunk.js"
+  },
+  {
+    "revision": "249b5bde9582b56fc0920cb7d4342e55",
+    "url": "/favorite-music-survey/static/media/guitar-bg.249b5bde.jpg"
+  },
+  {
+    "revision": "fd53d7ad40cc6e2e95c0ff24ed9f8164",
+    "url": "/favorite-music-survey/index.html"
+  }
+];
